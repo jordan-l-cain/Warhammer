c71114e1aebfb30c189b4d05dff751823d86ca99
Jordan <jordan.l.cain@gmail.com>
2017-05-19 14:17:58 -0700

    Initial Commit

---
+++ Warhammer/Source/Warhammer/WarhammerGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "WarhammerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARHAMMER_API AWarhammerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
